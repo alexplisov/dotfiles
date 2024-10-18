ec294f856830bf22e764a74825cbea9f76c6e056
Alexey Plisov <alexplisov@gmail.com>
2024-10-18 20:26:25 +0500

    upd

--- configs/nvim/lua/config/lazy/lualine.lua
+++ configs/nvim/lua/config/lazy/lualine.lua
@@ -8,6 +8,24 @@ return {
 				component_separators = { left = '', right = '' },
 				section_separators = { left = '', right = '' },
 			},
+			sections = {
+				lualine_a = {
+					{
+						'VimLogo',
+						fmt = function()
+							return ''
+						end,
+						padding = { left = 1, right = 0 }
+					},
+					{
+						'mode',
+						fmt = function(str)
+							return str
+						end,
+						padding = { left = 1, right = 1 },
+					}
+				}
+			}
 		}
 	end
 }
